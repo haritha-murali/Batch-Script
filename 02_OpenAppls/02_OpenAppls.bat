d761985c70c4c45d526cd71c55d2070837279c64
haritha-murali <88222222+haritha-murali@users.noreply.github.com>
2024-11-18 18:32:25 +0530

    Add files via upload

---
+++ 02_OpenAppls/02_OpenAppls.bat
@@ -0,0 +1,13 @@
+@echo off
+
+title 02_OpenAppls
+
+echo This is batch script to open URLs
+
+pause
+
+start "C:\Program Files\Mythicsoft\Agent Ransack" AgentRansack.exe"
+start notepad.exe
+
+
+exit
